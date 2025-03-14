6a0eb67da0055d90ebcebdf142d1a5c260f52ba2
StaTicDKK17 <tobse2002@gmail.com>
2025-03-14 18:22:11 +0100

    Stop tracking workspace.json

---
+++ omicron_os/target/debug/deps/omicron_os-074b214c2ef50189.d
@@ -0,0 +1,5 @@
+D:\Project-Omicron\omicron_os\target\debug\deps\libomicron_os-074b214c2ef50189.rmeta: src/main.rs
+
+D:\Project-Omicron\omicron_os\target\debug\deps\omicron_os-074b214c2ef50189.d: src/main.rs
+
+src/main.rs:
