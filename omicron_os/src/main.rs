d05a003e52d35ac18e064ca58a5c86fe07054c7c
StaTicDKK17 <tobse2002@gmail.com>
2025-03-15 10:45:18 +0100

    Added simple testing to omicron_os

--- omicron_os/src/main.rs
+++ omicron_os/src/main.rs
@@ -1,20 +1,32 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
-
-mod vga_buffer;
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(omicron_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use omicron_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    loop {}
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    omicron_os::test_panic_handler(info)
 }
