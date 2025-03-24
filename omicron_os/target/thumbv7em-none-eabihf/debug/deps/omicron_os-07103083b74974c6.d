75d3117291b751c14f55ef0375429b826b30506b
StaTicDKK17 <tobse2002@gmail.com>
2025-03-24 18:03:43 +0100

    I do not remember what I did here

--- omicron_os/target/thumbv7em-none-eabihf/debug/deps/omicron_os-07103083b74974c6.d
+++
@@ -1,5 +0,0 @@
-D:\Project-Omicron\omicron_os\target\thumbv7em-none-eabihf\debug\deps\omicron_os-07103083b74974c6: src/main.rs
-
-D:\Project-Omicron\omicron_os\target\thumbv7em-none-eabihf\debug\deps\omicron_os-07103083b74974c6.d: src/main.rs
-
-src/main.rs:
--- omicron_os/target/thumbv7em-none-eabihf/debug/omicron_os.d
+++
@@ -1,1 +0,0 @@
-D:\Project-Omicron\omicron_os\target\thumbv7em-none-eabihf\debug\omicron_os: D:\Project-Omicron\omicron_os\src\main.rs
